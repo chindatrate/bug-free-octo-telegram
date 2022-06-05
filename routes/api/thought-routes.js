d8d139fc63734a9b59de9933aa1bf576c15015c2
chinda trate <chindatrate@me.com>
2022-06-05 19:05:34 -0400

    update routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+  addThought,
+  getThoughts,
+  getThoughtById,
+  updateThoughtById,
+  deleteThoughtById,
+  addReaction,
+  deleteReaction
+} = require('../../controllers/thoughts-controller');
+
+router
+  .route('./:thoughtId')
+  .get(getThoughtById)
+  .put(updateThoughtById)
+  .delete(deleteThoughtById);
+
+router.route('/').get(getThoughts).post(addThought);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+  addFriend,
+  deleteFriendById
+} = require('../../controllers/users-controller');
+
+router
+  .route('./:userId')
+  .get(getUserById)
+  .put(updateUserById)
+  .delete(deleteUserById);
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:UserId/friends/:friendId').delete(deleteFriendById.post(addFriend));
+
+module.exports = router;
